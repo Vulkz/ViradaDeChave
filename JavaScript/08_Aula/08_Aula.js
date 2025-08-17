af9f7d8350dfef2b1349dabc3561a78d530e9281
vulkz <gabrielcosta3536@gmail.com>
2025-08-17 14:01:11 -0300

    Aula 08 - Operadores aritméticos

---
+++ JavaScript/08_Aula/08_Aula.js
@@ -0,0 +1,18 @@
+/**
+ *  Aritméticos
+ *  +  | Adição - Concatenação
+ *  ++ | Incrementação
+ *  -  | Subtração
+ *  -- | Decrementação
+ *  /  | Divisão
+ *  *  | Multiplicação
+ *  ** | Potenciação 
+ *  %  | Resto da divisão
+ *  += | Operador de atribuição
+ * NaN | Not a number
+ */
+
+const num1 = parseInt('5'); // Passa uma string para inteiro
+const num2 = 10;
+
+console.log(num1 + num2);
