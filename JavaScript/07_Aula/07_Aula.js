cba48601ddad6d252332a3058b00f55d8bc137dc
vulkz <gabrielcosta3536@gmail.com>
2025-08-17 13:31:25 -0300

    Aula 07 - tipos de dados primitivos

---
+++ JavaScript/07_Aula/07_Aula.js
@@ -0,0 +1,12 @@
+const nome = 'Gabriel'; // String
+const num = 22; // Number
+const sobreNomeAluno = null; // Null -> não aponta para local nenhum na memória explicitamente
+const aprovado = true; // Boolean -> true, false (lógico)
+
+let nomeAluno; // Undefined -> não aponta para local nenhum na memória 
+
+console.log(typeof(nome));
+console.log(typeof(num));
+console.log(typeof(sobreNomeAluno));
+console.log(typeof(aprovado));
+console.log(typeof(nomeAluno));
