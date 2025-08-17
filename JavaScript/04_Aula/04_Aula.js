c8e7f1cbf2085506b02d5f31915eb678b8aee693
vulkz <gabrielcosta3536@gmail.com>
2025-08-16 22:31:00 -0300

    Aula 04 - Variáveis com let

---
+++ JavaScript/04_Aula/04_Aula.js
@@ -0,0 +1,13 @@
+/**
+ * Não podemos criar variáveis com palavras reservadas
+ * Não pode começar o nome de uma váriavel com um número
+ * Não podem conter espaços ou traços
+ * Variáveis precisam ter nomes significativos
+ * Utilizamos camelCase
+ * Case-sensitive
+ */
+
+let nome; // Declarou a variável
+nome = 'João'; // Inicializando a variável
+
+console.log(nome);
