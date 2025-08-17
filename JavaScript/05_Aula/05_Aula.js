a66368b6d4e5b780e3fc49a2626448a0c0e81081
vulkz <gabrielcosta3536@gmail.com>
2025-08-16 22:46:10 -0300

    Aula 05 - Constantes com const

---
+++ JavaScript/05_Aula/05_Aula.js
@@ -0,0 +1,20 @@
+/**
+ * Não podemos criar variáveis com palavras reservadas
+ * Não pode começar o nome de uma váriavel com um número
+ * Não podem conter espaços ou traços
+ * Não podemos modificar o valor de uma constante
+ * Variáveis precisam ter nomes significativos
+ * Utilizamos camelCase
+ * Case-sensitive
+ */
+
+
+const nome = 'João'; 
+
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultadoSoma = primeiroNumero + segundoNumero;
+
+console.log(resultadoSoma, typeof(resultadoSoma));
