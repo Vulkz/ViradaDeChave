245bb5006027b2db61e54959ac24d09465492b75
vulkz <gabrielcosta3536@gmail.com>
2025-08-17 21:48:16 -0300

    Exercicio

--- JavaScript/09_Aula/Exercicio/scripts.js
+++ JavaScript/09_Aula/Exercicio/scripts.js
@@ -1,6 +1,6 @@
-const num1 = window.prompt('Digite um número');
-const num2 = window.prompt('Digite outro número');
+let num1 = window.prompt('Digite um número');
+let num2 = window.prompt('Digite outro número');
 
 const resultado = Number(num1) + Number(num2);
 
-window.alert(resultado);
+window.alert(`O resultado da soma de ${num1} e ${num2} é: ${resultado}`);
