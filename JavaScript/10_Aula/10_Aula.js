41f2e2b28facdbf81caf263eacaa5b6b20920027
vulkz <gabrielcosta3536@gmail.com>
2025-08-18 20:27:45 -0300

    Mais sobre strings

---
+++ JavaScript/10_Aula/10_Aula.js
@@ -0,0 +1,13 @@
+let caracterDeEscape = "Um \"texto\".";
+
+console.log(caracterDeEscape.indexOf("\"texto\"")); //Mostra o indice da palavra "texto"
+console.log(caracterDeEscape.length); // Mostra o tamanho da string
+console.log(caracterDeEscape.replace('texto', 'pato')); // Troca a palavra 'texto' pela palavra 'pato'
+
+console.log(caracterDeEscape.slice(3, 11)); // Seleciona um intervalo 
+console.log(caracterDeEscape.slice(-8, 11)); // Seleciona um intervalo 
+
+console.log(caracterDeEscape.split(' ')); // Passa uma string para array
+
+console.log(caracterDeEscape.toUpperCase());
+console.log(caracterDeEscape.toLowerCase());
