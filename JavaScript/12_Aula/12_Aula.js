852cab838b41bfce08ea66f0f5f0ad74266e554b
vulkz <gabrielcosta3536@gmail.com>
2025-08-20 15:49:15 -0300

    Aula 12 - Math

---
+++ JavaScript/12_Aula/12_Aula.js
@@ -0,0 +1,14 @@
+let num = 9.512314;
+
+let arredondaParaBaixo = Math.floor(num); // Arredonda para baixo
+let arredondaParaCima = Math.ceil(num); // Arredonda para cima
+let maisProximo = Math.round(num); // Arredonda para o número mais próximo
+
+let maior = Math.max(1, 2, 3, 10, 4, 5); // Pega o maior número
+let menor = Math.min(1, 2, 3, -5, 6, 7); // Pega o menor número 
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Gera um número aleatório entre 10 e 5
+
+let pi = Math.PI; // Imprime o número de PI
+let raizQuadrada = num ** 0.5; // Pega a raiz quadrada
+let infinito = 100 / 0; // Gera infinity e é positivo
