9bcf30e5c24ae58e5c92adec750c43b6a8c9991a
vulkz <gabrielcosta3536@gmail.com>
2025-09-01 23:36:52 -0300

    Aula 18 - Operadores logicos

---
+++ JavaScript/18_Aula/scripts.js
@@ -0,0 +1,20 @@
+/**
+ * Operadores lógicos
+ * && -> AND -> E
+ * || -> OR -> OU
+ * ! -> NOT -> NÃO
+ */
+
+//AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+//OR
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+
+//NOT
+console.log(!true);
+console.log(!false);
