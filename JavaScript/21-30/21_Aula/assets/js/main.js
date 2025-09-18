02f33533be7d21520bd59c0a52513624bb427b34
Gabriel Costa <gabrielcosta3536@gmail.com>
2025-09-17 22:39:49 -0300

    Exercicio

--- JavaScript/21-30/21_Aula/assets/js/main.js
+++ JavaScript/21-30/21_Aula/assets/js/main.js
@@ -6,13 +6,36 @@ function calcularIMC() {
     try {
         if (!peso || !altura) throw 'Por favor, preencha todos os campos.';
         if (isNaN(peso) || isNaN(altura)) throw 'Por favor, insira valores numéricos.';
+        
         peso = parseFloat(peso);
         altura = parseFloat(altura);
+
         const imc = peso / (altura * altura);
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)}`;
+        const resultadoCalculado = resultadoIMC(imc);
+
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)} - ${resultadoCalculado}`;
         resultado.style.display = 'block';
+
     } catch (error) {
         resultado.innerHTML = error;
         resultado.style.display = 'block';
+    }
+}
+
+function resultadoIMC(imc) {
+    let resultado = imc;
+    
+    if (resultado < 18.5) {
+        return 'Abaixo do peso';
+    } else if (resultado >= 18.5 && resultado < 24.9) {
+        return 'Peso normal';
+    } else if (resultado >= 25 && resultado < 29.9) {
+        return 'Sobrepeso';
+    } else if (resultado >= 30 && resultado < 34.9) {
+        return 'Obesidade grau I';
+    } else if (resultado >= 35 && resultado < 39.9) {
+        return 'Obesidade grau II';
+    } else {
+        return 'Obesidade grau III';
     }
 }
